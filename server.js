0c271ce9dde4d49335216f9d9d6a1fa15f4ba7a0
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-09-24 15:26:53 +0500

    test with rest_Api

--- server.js
+++ server.js
@@ -1,10 +1,88 @@
+const { stat } = require('fs')
 const http = require('http')
+const { url } = require('inspector')
+const { buffer } = require('node:stream/consumers')
+
+const todos =[{
+    id:"1",name:"todo One",
+    description:"Ok"
+},
+{
+    id:"2",name:"todo two",
+    description:"Ok"
+},
+{
+    id:"3",name:"todo three",
+    description:"Ok"
+},
+{
+    id:"4",name:"todo four",
+    description:"Ok"
+}]
 
 const server = http.createServer((req,res)=>{
-    const{headers,url,method}=req;
-    console.log(headers,url,method)
+    const {method,url}= req;
+    
+    // const{headers,url,method}=req;
+    // console.log(headers,url,method)
+    // res.statusCode=404;
+   
+  
+    // res.write('<h1>Hello</h1>')(
+
+    let body = [];
+   
+
+    req.on('data',chunk =>{
+        body.push(chunk);
+    }).on('end',()=>{
+
+        
+        body=Buffer.concat(body).toString();
+
+        let status = 404;
+        let response = {
+            success : false,
+            data: null,
+            error:null
+        }
+    
+        if(method==='GET'&&url==='/todos'){
+            status = 200;
+            response.success= true;
+            response.data=todos;
+        }
+        else if(method==='POST'&&url==='/todos'){
+            
+            const{id,name,description}=JSON.parse(body)
+            if((!id)||(!name)||(!description)){
+                status =400;
+                response.error="Please enter all fields"
+            }else{
+               todos.push({id,name,description})
+                status = 201;
+                response.success=true;
+                response.data=todos;
+            }
+            
+        }
+
+        res.writeHead(status,{
+            'content-Type':' /json',
+            'X-Powered-By':'Node.js'
+         })
+         res.end(JSON.stringify(response)); 
+    })
+  
+
+  
+  
+
+   
+    
+
     console.log(req)
-    res.end();
+   
 })
 
 
